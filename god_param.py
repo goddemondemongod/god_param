8231ed0c3f83ff7c7ce8f0e5f18d24b29e298e95
goddemondemongod <66908237+goddemondemongod@users.noreply.github.com>
2023-11-27 16:21:11 +0800

    Update god_param.py

--- god_param.py
+++ god_param.py
@@ -14,6 +14,7 @@ from java.awt import BorderLayout, Font, Color
 
 from java.io import PrintWriter
 
+pattern = r'[?&/.;]'
 regex = {
     "Linker": r'(?:"|\')((?!text\/javascript)((?:[a-zA-Z]{1,10}://|//)[^"\'/]{1,}\.[a-zA-Z]{2,}[^"\']{0,})|((?:/|\.\./|\./)[^"\'><,;|*()(%%$^/\\\[\]][^"\'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|#][^"|\']{0,}|))|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{3,}(?:[\?|#][^"|\']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:php|asp|aspx|jsp|json|action|html|js|txt|xml)(?:[\?|#][^"|\']{0,}|)))(?:"|\')',
 }
@@ -24,6 +25,10 @@ def getSensitiveParamsFromFile():
         return [line.strip() for line in spf.readlines()]
 
 class BurpExtender(IBurpExtender, ITab,IHttpListener,IExtensionStateListener):
+    def __init__(self):
+        self.target_whitelist = []  # 添加你的白名单目标
+
+
     def registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
         self._helpers = callbacks.getHelpers()
@@ -53,6 +58,12 @@ class BurpExtender(IBurpExtender, ITab,IHttpListener,IExtensionStateListener):
         return routes  
 
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
+        service = messageInfo.getHttpService()
+        host = service.getHost()
+        
+        # 如果目标主机不在白名单中，直接返回
+        if host not in self.target_whitelist:
+            return        
         if messageIsRequest :
             self.requestParamDict['urlParams'] = []
             self.requestParamDict['bodyParams'] = []
@@ -180,65 +191,108 @@ class BurpExtender(IBurpExtender, ITab,IHttpListener,IExtensionStateListener):
 
 
 
+    def startWhitelist(self, event):
+        if self.startWhitelistBtn.getText() == "Start Whitelist":
+            targets_text = self.targetInput.getText().strip()
+            targets_text = str(targets_text)
+            targets = targets_text.split(";")
+            targets = [target.strip() for target in targets if target.strip()]
+            self.target_whitelist = targets
+            print("启动白名单：{}".format(self.target_whitelist))
+            self.startWhitelistBtn.setText("Stop Whitelist")
+        else:
+        # 处理停止白名单的逻辑
+        # 可以添加一些代码来清空白名单或者停止相关功能
+            self.startWhitelistBtn.setText("Start Whitelist")
+
+
+
+
+
+
+
+
+
+
+
+
+
     def getUiComponent(self):
         defaultCharset = locale.getdefaultlocale()[1]
         self.spePanel = JPanel()
         self.spePanel.setLayout(None)
+
+        # 日志区域
         self.logPane = JScrollPane()
         self.outputTxtArea = JTextArea()
         self.outputTxtArea.setFont(Font("Consolas", Font.PLAIN, 12))
         self.outputTxtArea.setLineWrap(True)
         self.logPane.setViewportView(self.outputTxtArea)
-        self.logPane.setBounds(20,50,800,600)
+        self.logPane.setBounds(20, 50, 800, 600)
         self.spePanel.add(self.logPane)
 
+        # 按钮区域
         self.clearBtn = JButton("Clear", actionPerformed=self.clearRst)
-        self.readBtn = JButton("Read", actionPerformed=self.readRst)#进行装载函数
+        self.readBtn = JButton("Read", actionPerformed=self.readRst)
         self.exportBtn = JButton("Export", actionPerformed=self.exportRst)
-        self.parentFrm = JFileChooser()
-        self.clearBtn.setBounds(20,650,100,30)
-        self.readBtn.setBounds(300,650,100,30)
-        self.exportBtn.setBounds(600,650,100,30)
+        #self.startBtn = JButton("Start", actionPerformed=self.startRst)
+
+        self.clearBtn.setBounds(20, 670, 100, 30)
+        self.readBtn.setBounds(120, 670, 100, 30)
+        self.exportBtn.setBounds(220, 670, 100, 30)
+        #self.startBtn.setBounds(320, 670, 100, 30)
         self.spePanel.add(self.clearBtn)
-        self.spePanel.add(self.readBtn)        
+        self.spePanel.add(self.readBtn)
         self.spePanel.add(self.exportBtn)
+        #self.spePanel.add(self.startBtn)
+
+        # 白名单输入框和按钮
+        self.targetInput = JTextField(20)
+        self.startWhitelistBtn = JButton("Start Whitelist", actionPerformed=self.startWhitelist)
+
+        self.targetInput.setBounds(520, 670, 150, 30)
+        self.startWhitelistBtn.setBounds(675, 670, 150, 30)
+
+        self.spePanel.add(self.targetInput)
+        self.spePanel.add(self.startWhitelistBtn)
+
+        # 敏感参数区域
         self.sensitiveParamsRegularTextArea = JTextArea('\n'.join(""))
         self.sensitiveParamsRegularScrollPane = JScrollPane(self.sensitiveParamsRegularTextArea)
-        self.sensitiveParamsRegularScrollPane.setBounds(850,50,150,600)
+        self.sensitiveParamsRegularScrollPane.setBounds(850, 50, 150, 600)
         self.spePanel.add(self.sensitiveParamsRegularScrollPane)
-       
+
+        # 信息展示区域
         self.textLabelAuthor = JLabel()
         self.textLabelAuthor.setText("Author: goddemon".encode(defaultCharset).decode(defaultCharset))
         self.textLabelAuthor.setHorizontalAlignment(SwingConstants.CENTER)
         self.textLabelAuthor.setVerticalAlignment(SwingConstants.TOP)
         self.textLabelAuthor.setBounds(1020, 200, 200, 20)
-        self.spePanel.add(self.textLabelAuthor)    
-
-
+        self.spePanel.add(self.textLabelAuthor)
 
         self.textLabelConsult = JLabel()
         self.textLabelConsult.setText("Consulttools:  ".encode(defaultCharset).decode(defaultCharset))
         self.textLabelConsult.setHorizontalAlignment(SwingConstants.CENTER)
         self.textLabelConsult.setVerticalAlignment(SwingConstants.TOP)
         self.textLabelConsult.setBounds(1020, 230, 200, 60)
-        self.spePanel.add(self.textLabelConsult)   
+        self.spePanel.add(self.textLabelConsult)
 
         self.textLabelConsult = JLabel()
         self.textLabelConsult.setText("burp-sensitive-param-extractor ".encode(defaultCharset).decode(defaultCharset))
         self.textLabelConsult.setHorizontalAlignment(SwingConstants.CENTER)
         self.textLabelConsult.setVerticalAlignment(SwingConstants.TOP)
         self.textLabelConsult.setBounds(1020, 250, 200, 60)
-        self.spePanel.add(self.textLabelConsult)    
+        self.spePanel.add(self.textLabelConsult)
 
         self.textLabelConsultAuthor = JLabel()
         self.textLabelConsultAuthor.setText("Thanks: LSA".encode(defaultCharset).decode(defaultCharset))
         self.textLabelConsultAuthor.setHorizontalAlignment(SwingConstants.CENTER)
         self.textLabelConsultAuthor.setVerticalAlignment(SwingConstants.TOP)
         self.textLabelConsultAuthor.setBounds(1020, 270, 200, 60)
-        self.spePanel.add(self.textLabelConsultAuthor)    
-
+        self.spePanel.add(self.textLabelConsultAuthor)
 
         self.alertSaveSuccess = JOptionPane()
         self.spePanel.add(self.alertSaveSuccess)
 
         return self.spePanel
+
